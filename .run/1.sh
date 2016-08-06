174c5b1bb52e0fdf86b035b114d6404635d9654c
Dr.Cyber <amirhosseinaghajani@hotmail.com>
2016-08-07 00:34:27 +0430

    Update 1.sh

--- .run/1.sh
+++ .run/1.sh
@@ -13,6 +13,6 @@ adv() {
  echo "Use:"
  echo "$0 r"
  #This is a helper for create anti spam bot.
- #created by: @janlou and powered by: @AdvanTm
+ #created by: @DrCyber and powered by: CyberTeam
  #warning: CopyRight all right reserved, we can block you channel or bot.
 fi
